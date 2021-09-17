1bd5ab53ec835585833aeee30e86f31c2a20e64d
qhhivan <orsolic.i02@htlwienwest.at>
2021-09-17 14:31:34 +0200

    Start

---
+++ client/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-navigation-drawer class="green" app>
+      <span class="white--text title">v-navigation-drawer</span>
+    </v-navigation-drawer>
+    <v-system-bar class="pink" app>
+      <span class="white--text title">v-system-bar</span>
+    </v-system-bar>
+    <v-app-bar class="yellow" app>
+      <span class="title">v-app-bar</span>
+    </v-app-bar>
+    <v-main class="blue">
+      <span class="white--text title">v-main</span>
+    </v-main>
+    <v-footer class="purple" app>
+      <span class="white--text title mx-auto">v-footer</span>
+    </v-footer>
+    <v-bottom-navigation class="cyan" app>
+      <span class="white--text title">v-bottom-navigation</span>
+    </v-bottom-navigation>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
